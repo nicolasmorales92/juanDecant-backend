f842fad21b8e690c9049da8b62a7a17a21ba13ba
nicomorales <nico.ememem@gmail.com>
2026-09-09 21:27:30 -0300

    correccion de front url

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   const seeder = app.get(SeederService);
   await seeder.seederProductos();
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'https://juandecant.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
