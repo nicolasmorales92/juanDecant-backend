204001ccde8252afe563df6fa2295f9a49f9daa5
nicomorales <nico.ememem@gmail.com>
2026-09-18 01:12:35 -0300

    me olvide del cors (japishhh)

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
   const seeder = app.get(SeederService);
   await seeder.seederProductos();
   app.enableCors({
-    origin:   'http://localhost:3000',
+    origin:  process.env.FRONTEND_URL || 'https://juanparfum.vercel.app' || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
